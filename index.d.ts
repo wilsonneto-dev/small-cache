c67501ebe8d06c5999b89304f404012e493db258
Wilson Neto <contato@wilsonneto.com.br>
2021-09-12 02:38:58 -0300

    fix: removing distribution files

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-interface ICacheOptions {
-    TTL_InSeconds: number;
-    enabled: boolean;
-}
-declare const getFromCache: <T>(key: string, fetchFunction: () => Promise<T>, options?: ICacheOptions | null) => Promise<T>;
-export { getFromCache };
